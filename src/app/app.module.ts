2d9af0d319fc500d31ae3992fb170c76cb1e29ae
PeterProfile <835887626@qq.com>
2019-05-09 20:57:52 +0800

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ],
   providers: [HeroService, MessageService]
 })
